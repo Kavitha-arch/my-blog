fc42dfeb5d605ad4b3d8d0b2b9be81b7dce872b9
Kavitha-arch <kavithanagaraj.p@gmail.com>
2026-02-08 17:24:44 +0530

    Deploy blog to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/my-blog',
+  assetPrefix: "/my-blog/",
+  images: { unoptimized: true },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
